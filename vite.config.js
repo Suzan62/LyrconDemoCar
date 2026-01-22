af74944dc5322057c6da9661c799b3e798d45691
Suzan62 <suzanchaklasiya810@gmail.com>
2026-01-22 18:10:24 +0530

    Add proxy config for preview mode

--- vite.config.js
+++ vite.config.js
@@ -17,5 +17,14 @@ export default defineConfig({
         secure: false,
       }
     }
+  },
+  preview: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
